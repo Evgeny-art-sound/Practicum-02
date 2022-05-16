ca1ff606f5847e362eaddf239b5789782d691322
Sharaborin Evgeny <art-sound45@yandex.ru>
2022-05-16 21:26:17 +0500

    add example 4

--- example04/Program.cs
+++ example04/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Найти расстояние между точками в пространстве 2D/3D
+Console.WriteLine("Введите количество плоскостей для расчета 2 или 3? ");
+int d = int.Parse(Console.ReadLine());
+if (d == 2)
+{
+	Console.Write("Введите координату X точки А ");
+	int ax = int.Parse(Console.ReadLine());
+	Console.Write("Введите координату Y точки А ");
+	int ay = int.Parse(Console.ReadLine());
+	Console.Write("Введите координату X точки B ");
+	int bx = int.Parse(Console.ReadLine());
+	Console.Write("Введите координату Y точки B ");
+	int by = int.Parse(Console.ReadLine());
+	Console.Write("Расстояние между точками А и B = ");
+	Console.Write(Math.Sqrt(Math.Pow((bx - ax), 2) + Math.Pow((by - ay), 2)));
+}
+else if (d == 3)
+{
+	Console.Write("Введите координату X точки А ");
+	int ax = int.Parse(Console.ReadLine());
+	Console.Write("Введите координату Y точки А ");
+	int ay = int.Parse(Console.ReadLine());
+	Console.Write("Введите координату Z точки А ");
+	int az = int.Parse(Console.ReadLine());
+	Console.Write("Введите координату X точки B ");
+	int bx = int.Parse(Console.ReadLine());
+	Console.Write("Введите координату Y точки B ");
+	int by = int.Parse(Console.ReadLine());
+	Console.Write("Введите координату Z точки B ");
+	int bz = int.Parse(Console.ReadLine());
+
+	Console.Write("Расстояние между точками А и B = ");
+	Console.Write(Math.Sqrt(Math.Pow((bx - ax), 2) + Math.Pow((by - ay), 2) + Math.Pow((bz - az), 2)));
+}
+else Console.WriteLine("Число плоскостей может быть только 2 или 3 !");
