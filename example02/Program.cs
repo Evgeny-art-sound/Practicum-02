a5ff363e6a56110f42d315b48f8ea83d8e878c24
Evgeny-art-sound <103237442+Evgeny-art-sound@users.noreply.github.com>
2022-05-16 21:51:00 +0500

    Update Program.cs
    fix bugs

--- example02/Program.cs
+++ example02/Program.cs
@@ -1,14 +1,14 @@
 ﻿// По двум заданным числам проверять является ли одно квадратом другого
-Console.Write("Введите первое чило ");
+Console.Write("Введите первое чиcло ");
 int a = int.Parse(Console.ReadLine());
 Console.Write("Введите второе число ");
 int b = int.Parse(Console.ReadLine());
 if (b * b == a)
 {
-	Console.WriteLine("Первое чило квадрат второго ");
+	Console.WriteLine("Первое чиcло квадрат второго ");
 }
 else if (a * a == b)
 {
 	Console.WriteLine("Второе число квадрат первого ");
 }
-else Console.WriteLine("Числа не являются квадратами друг-друга");
+else Console.WriteLine("Одно число не квадрат второго ");
